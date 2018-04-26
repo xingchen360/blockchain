c8907691c82406af28c1b24752dcfb9450c7de82
love-somnus <928200207@qq.com>
2018-04-26 10:27:32 +0800

    love-somnus <928200207@qq.com>

--- src/main/resources/contract.sol
+++ src/main/resources/contract.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.4.0;
 
 contract Register{
-    struct Administrators{
-        address ownerAdr;
+	struct Administrators{
+		address ownerAdr;
         string ownerName;
         bytes32 password;
     }
@@ -12,16 +12,17 @@ contract Register{
     mapping(string=>bool) registerPool;
     mapping(string => Administrators) userPool;
     
-     function Register() public {
-        owner = msg.sender;
+	function Register() public {
+		owner = msg.sender;
     }
+    
     //注册
-     function login(string username, string password) constant public returns (bool) {
+	function login(string username, string password) constant public returns (bool) {
         return userPool[username].password == keccak256(password);
     }
+    
     //注册检测
     function checkRegister(address addr,string username) constant public returns (bool) {
-	    
 		for(uint i = 0; i < addrList.length; ++i){
             if(addrList[i] == addr||registerPool[username]==true)
                 return true;
@@ -34,13 +35,13 @@ contract Register{
         require(!(msg.sender!=owner));
             
         require(!checkRegister(addr,username));
-        
             
 		userPool[username] = Administrators(addr, username, keccak256(password));
 		addrList.push(addr);
 		registerPool[username]=true;
 		++AdministratorsNum;
     }
+    
     // 更新密码
     function updatePassword(string username, string newPwd) public {
 		require(msg.sender != owner);
@@ -59,36 +60,42 @@ contract Register{
 
 contract AddorSearch{
     struct Msg{
-    string userName;
-    string ID;
-    string violateRecord;
-    uint lowPoint;
-    string AdministratorName;
+		string userName;
+		string ID;
+		string violateRecord;
+		uint lowPoint;
+		string AdministratorName;
     }
+    
     Msg[] violateRecords;
     uint totalMsg=0;
     
     function addMsg(string username,string ID,string violateRecord,uint lowPoint,string AdministratorName ) public{
         violateRecords.push(Msg(username,ID,violateRecord,lowPoint,AdministratorName));
     }
+    
     function returnTotal() constant public returns (uint) {
         return violateRecords.length;
     }
+    
     function getuserName(uint id) constant public returns (string){
        return violateRecords[id].userName;
     }
+    
     function getID(uint id) constant public returns (string){
         return violateRecords[id].ID;
     }
+    
     function getviolateRecord(uint id) constant public returns (string){
         return violateRecords[id].violateRecord;
     }
+    
     function getlowPoint(uint id) constant public returns (uint){
         return violateRecords[id].lowPoint;
     }
+    
     function getAdministrator(uint id) constant public returns (string){
         return violateRecords[id].AdministratorName;
     }
-
     
 }
